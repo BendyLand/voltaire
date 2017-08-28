BendyLand/voltaire

commit 1c6c4142fe7614ade1217bfbfd644bb7d843dc38

Cleanup tons of obsolete commented out code
Mostly in EditorNode, dropping some obsolete editor plugins and also a
cleanup of ProjectSettings/EditorSettings.

editor/project_settings_editor.cpp
@@ -29,13 +29,13 @@
 /*************************************************************************/
 #include "project_settings_editor.h"
 
-#include "editor_node.h"
-#include "global_constants.h"
-#include "os/keyboard.h"
-#include "project_settings.h"
+#include "core/global_constants.h"
+#include "core/os/keyboard.h"
+#include "core/project_settings.h"
+#include "core/translation.h"
+#include "editor/editor_node.h"
 #include "scene/gui/margin_container.h"
 #include "scene/gui/tab_container.h"
-#include "translation.h"
 
 ProjectSettingsEditor *ProjectSettingsEditor::singleton = NULL;
 
@@ -184,8 +184,6 @@ void ProjectSettingsEditor::_device_input_add() {
 	int idx = edit_idx;
 	Variant old_val = ProjectSettings::get_singleton()->get(name);
 	Array arr = old_val;
-	//	ie.device = device_id->get_value();
-	//	ie.type = add_type;
 
 	switch (add_type) {
 
@@ -619,7 +617,6 @@ void ProjectSettingsEditor::_update_actions() {
 			continue;
 
 		TreeItem *item = input_editor->create_item(root);
-		//item->set_cell_mode(0,TreeItem::CELL_MODE_CHECK);
 		item->set_text(0, name);
 		item->add_button(0, get_icon("Add", "EditorIcons"), 1, false, TTR("Add Event"));
 		if (!ProjectSettings::get_singleton()->get_input_presets().find(pi.name)) {
@@ -627,7 +624,6 @@ void ProjectSettingsEditor::_update_actions() {
 			item->set_editable(0, true);
 		}
 		item->set_custom_bg_color(0, get_color("prop_subsection", "Editor"));
-		//item->set_checked(0,pi.usage&PROPERTY_USAGE_CHECKED);
 
 		Array actions = ProjectSettings::get_singleton()->get(pi.name);
 
@@ -748,19 +744,7 @@ void ProjectSettingsEditor::_item_add() {
 	}
 
 	String catname = category->get_text().strip_edges();
-	/*if (!catname.is_valid_identifier()) {
-		message->set_text("Invalid Category.\nValid characters: a-z,A-Z,0-9 or _");
-		message->popup_centered(Size2(300,100));
-		return;
-	}*/
-
 	String propname = property->get_text().strip_edges();
-	/*if (!propname.is_valid_identifier()) {
-		message->set_text("Invalid Property.\nValid characters: a-z,A-Z,0-9 or _");
-		message->popup_centered(Size2(300,100));
-		return;
-	}*/
-
 	String name = catname != "" ? catname + "/" + propname : propname;
 
 	undo_redo->create_action(TTR("Add Global Property"));
@@ -880,13 +864,6 @@ void ProjectSettingsEditor::_save() {
 	Error err = ProjectSettings::get_singleton()->save();
 	message->set_text(err != OK ? TTR("Error saving settings.") : TTR("Settings saved OK."));
 	message->popup_centered(Size2(300, 100) * EDSCALE);
-}
-
-void ProjectSettingsEditor::_settings_prop_edited(const String &p_name) {
-
-	String full_item = globals_editor->get_full_item_path(p_name);
-
-	_settings_changed();
 }
 
 void ProjectSettingsEditor::_settings_changed() {
@@ -1357,7 +1334,6 @@ void ProjectSettingsEditor::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("_add_item"), &ProjectSettingsEditor::_add_item, DEFVAL(Variant()));
 	ClassDB::bind_method(D_METHOD("_device_input_add"), &ProjectSettingsEditor::_device_input_add);
 	ClassDB::bind_method(D_METHOD("_press_a_key_confirm"), &ProjectSettingsEditor::_press_a_key_confirm);
-	ClassDB::bind_method(D_METHOD("_settings_prop_edited"), &ProjectSettingsEditor::_settings_prop_edited);
 	ClassDB::bind_method(D_METHOD("_copy_to_platform"), &ProjectSettingsEditor::_copy_to_platform);
 	ClassDB::bind_method(D_METHOD("_update_translations"), &ProjectSettingsEditor::_update_translations);
 	ClassDB::bind_method(D_METHOD("_translation_delete"), &ProjectSettingsEditor::_translation_delete);
@@ -1391,12 +1367,6 @@ ProjectSettingsEditor::ProjectSettingsEditor(EditorData *p_data) {
 	tab_container = memnew(TabContainer);
 	tab_container->set_tab_align(TabContainer::ALIGN_LEFT);
 	add_child(tab_container);
-	//set_child_rect(tab_container);
-
-	//tab_container->set_anchor_and_margin(MARGIN_LEFT,ANCHOR_BEGIN, 15 );
-	//tab_container->set_anchor_and_margin(MARGIN_RIGHT,ANCHOR_END, 15 );
-	//tab_container->set_anchor_and_margin(MARGIN_TOP,ANCHOR_BEGIN, 15 );
-	//tab_container->set_anchor_and_margin(MARGIN_BOTTOM,ANCHOR_END, 35 );
 
 	VBoxContainer *props_base = memnew(VBoxContainer);
 	props_base->set_alignment(BoxContainer::ALIGN_BEGIN);
@@ -1478,21 +1448,7 @@ ProjectSettingsEditor::ProjectSettingsEditor(EditorData *p_data) {
 	globals_editor->register_search_box(search_box);
 	globals_editor->get_property_editor()->get_scene_tree()->connect("cell_selected", this, "_item_selected");
 	globals_editor->get_property_editor()->connect("property_toggled", this, "_item_checked", varray(), CONNECT_DEFERRED);
-	globals_editor->get_property_editor()->connect("property_edited", this, "_settings_prop_edited");
-
-	/*
-	Button *save = memnew( Button );
-	props_base->add_child(save);
-
-	save->set_anchor(MARGIN_LEFT,ANCHOR_END);
-	save->set_anchor(MARGIN_RIGHT,ANCHOR_END);
-	save->set_anchor(MARGIN_TOP,ANCHOR_END);
-	save->set_anchor(MARGIN_BOTTOM,ANCHOR_END);
-	save->set_begin( Point2(80,28) );
-	save->set_end( Point2(10,20) );
-	save->set_text("Save");
-	save->connect("pressed",this,"_save");
-*/
+	globals_editor->get_property_editor()->connect("property_edited", this, "_settings_changed");
 
 	Button *del = memnew(Button);
 	hbc->add_child(del);
@@ -1506,16 +1462,6 @@ ProjectSettingsEditor::ProjectSettingsEditor(EditorData *p_data) {
 	popup_copy_to_feature->set_disabled(true);
 	add_prop_bar->add_child(popup_copy_to_feature);
 
-	/*List<StringName> ep;
-	EditorImportExport::get_singleton()->get_export_platforms(&ep);
-	ep.sort_custom<StringName::AlphCompare>();
-
-	for(List<StringName>::Element *E=ep.front();E;E=E->next()) {
-
-		popup_copy_to_feature->get_popup()->add_item( E->get() );
-
-	}*/
-
 	popup_copy_to_feature->get_popup()->connect("id_pressed", this, "_copy_to_platform");
 	popup_copy_to_feature->get_popup()->connect("about_to_show", this, "_copy_to_platform_about_to_show");
 
@@ -1524,7 +1470,6 @@ ProjectSettingsEditor::ProjectSettingsEditor(EditorData *p_data) {
 
 	message = memnew(ConfirmationDialog);
 	add_child(message);
-	//message->get_cancel()->hide();
 	message->set_hide_on_ok(true);
 
 	Control *input_base = memnew(Control);
@@ -1591,7 +1536,6 @@ ProjectSettingsEditor::ProjectSettingsEditor(EditorData *p_data) {
 
 	hbc = memnew(HBoxContainer);
 	device_input->add_child(hbc);
-	//device_input->set_child_rect(hbc);
 
 	VBoxContainer *vbc_left = memnew(VBoxContainer);
 	hbc->add_child(vbc_left);
@@ -1616,18 +1560,6 @@ ProjectSettingsEditor::ProjectSettingsEditor(EditorData *p_data) {
 	device_index = memnew(OptionButton);
 	vbc_right->add_child(device_index);
 
-	/*
-	save = memnew( Button );
-	input_base->add_child(save);
-	save->set_anchor(MARGIN_LEFT,ANCHOR_END);
-	save->set_anchor(MARGIN_RIGHT,ANCHOR_END);
-	save->set_anchor(MARGIN_TOP,ANCHOR_END);
-	save->set_anchor(MARGIN_BOTTOM,ANCHOR_END);
-	save->set_begin( Point2(80,28) );
-	save->set_end( Point2(10,20) );
-	save->set_text("Save");
-	save->connect("pressed",this,"_save");
-*/
 	setting = false;
 
 	//translations
@@ -1739,14 +1671,4 @@ ProjectSettingsEditor::ProjectSettingsEditor(EditorData *p_data) {
 	add_child(timer);
 
 	updating_translations = false;
-
-	/*
-	Control * es = memnew( Control );
-	es->set_name("Export");
-	tab_container->add_child(es);
-	export_settings = memnew( ProjectExportSettings );
-	es->add_child(export_settings);
-	export_settings->set_area_as_parent_rect();
-	export_settings->set_anchor_and_margin(MARGIN_BOTTOM,ANCHOR_END, 35 );
-*/
 }

editor/project_settings_editor.h
@@ -30,17 +30,16 @@
 #ifndef PROJECT_SETTINGS_H
 #define PROJECT_SETTINGS_H
 
-#include "editor_autoload_settings.h"
-#include "editor_data.h"
-#include "editor_plugin_settings.h"
-#include "property_editor.h"
+#include "core/undo_redo.h"
+#include "editor/editor_autoload_settings.h"
+#include "editor/editor_data.h"
+#include "editor/editor_plugin_settings.h"
+#include "editor/property_editor.h"
 #include "scene/gui/dialogs.h"
 #include "scene/gui/tab_container.h"
-#include "undo_redo.h"
-
-//#include "project_export_settings.h"
 
 class ProjectSettingsEditor : public AcceptDialog {
+
 	GDCLASS(ProjectSettingsEditor, AcceptDialog);
 
 	enum InputType {
@@ -122,10 +121,8 @@ class ProjectSettingsEditor : public AcceptDialog {
 	void _press_a_key_confirm();
 	void _show_last_added(const Ref<InputEvent> &p_event, const String &p_name);
 
-	void _settings_prop_edited(const String &p_name);
 	void _settings_changed();
 
-	//ProjectExportSettings *export_settings;
 	void _copy_to_platform(int p_which);
 
 	void _translation_file_open();

core/io/resource_format_binary.cpp
@@ -28,12 +28,14 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 #include "resource_format_binary.h"
-#include "image.h"
-#include "io/file_access_compressed.h"
-#include "io/marshalls.h"
-#include "os/dir_access.h"
-#include "project_settings.h"
-#include "version.h"
+
+#include "core/image.h"
+#include "core/io/file_access_compressed.h"
+#include "core/io/marshalls.h"
+#include "core/os/dir_access.h"
+#include "core/project_settings.h"
+#include "core/version.h"
+
 //#define print_bl(m_what) print_line(m_what)
 #define print_bl(m_what)
 
@@ -1798,7 +1800,6 @@ Error ResourceFormatSaverBinaryInstance::save(const String &p_path, const RES &p
 	}
 
 	ERR_FAIL_COND_V(err, err);
-	FileAccessRef _fref(f);
 
 	relative_paths = p_flags & ResourceSaver::FLAG_RELATIVE_PATHS;
 	skip_editor = p_flags & ResourceSaver::FLAG_OMIT_EDITOR_PROPERTIES;
@@ -1810,7 +1811,6 @@ Error ResourceFormatSaverBinaryInstance::save(const String &p_path, const RES &p
 		takeover_paths = false;
 
 	local_path = p_path.get_base_dir();
-	//bin_meta_idx = get_string_index("__bin_meta__"); //is often used, so create
 
 	_find_resources(p_resource, true);
 
@@ -1836,7 +1836,6 @@ Error ResourceFormatSaverBinaryInstance::save(const String &p_path, const RES &p
 		return ERR_CANT_CREATE;
 	}
 
-	//f->store_32(saved_resources.size()+external_resources.size()); // load steps -not needed
 	save_unicode_string(p_resource->get_class());
 	uint64_t md_at = f->get_pos();
 	f->store_64(0); //offset to impoty metadata
@@ -1875,7 +1874,6 @@ Error ResourceFormatSaverBinaryInstance::save(const String &p_path, const RES &p
 
 	f->store_32(strings.size()); //string table size
 	for (int i = 0; i < strings.size(); i++) {
-		//print_bl("saving string: "+strings[i]);
 		save_unicode_string(strings[i]);
 	}
 
@@ -1944,9 +1942,8 @@ Error ResourceFormatSaverBinaryInstance::save(const String &p_path, const RES &p
 	}
 
 	Vector<uint64_t> ofs_table;
-	//int saved_idx=0;
-	//now actually save the resources
 
+	//now actually save the resources
 	for (List<ResourceData>::Element *E = resources.front(); E; E = E->next()) {
 
 		ResourceData &rd = E->get();

editor/code_editor.cpp
@@ -1228,7 +1228,6 @@ CodeTextEditor::CodeTextEditor() {
 	error->set_valign(Label::VALIGN_CENTER);
 	error->add_color_override("font_color", Color(1, 0.7, 0.6, 0.9));
 	error->set_h_size_flags(SIZE_EXPAND_FILL); //required for it to display, given now it's clipping contents, do not touch
-	//status_bar->add_spacer();
 
 	Label *line_txt = memnew(Label);
 	status_bar->add_child(line_txt);

editor/editor_node.h
@@ -35,16 +35,35 @@
 #include "editor/create_dialog.h"
 #include "editor/editor_about.h"
 #include "editor/editor_data.h"
+#include "editor/editor_export.h"
+#include "editor/editor_log.h"
+#include "editor/editor_name_dialog.h"
 #include "editor/editor_path.h"
+#include "editor/editor_plugin.h"
+#include "editor/editor_reimport_dialog.h"
+#include "editor/editor_resource_preview.h"
 #include "editor/editor_run.h"
+#include "editor/editor_run_native.h"
+#include "editor/editor_run_script.h"
+#include "editor/editor_scale.h"
+#include "editor/editor_sub_scene.h"
 #include "editor/export_template_manager.h"
+#include "editor/fileserver/editor_file_server.h"
 #include "editor/filesystem_dock.h"
 #include "editor/groups_editor.h"
 #include "editor/import_dock.h"
 #include "editor/node_dock.h"
+#include "editor/pane_drag.h"
+#include "editor/progress_dialog.h"
+#include "editor/project_export.h"
+#include "editor/project_settings_editor.h"
 #include "editor/property_editor.h"
+#include "editor/quick_open.h"
 #include "editor/reparent_dialog.h"
+#include "editor/run_settings_dialog.h"
+#include "editor/scene_tree_dock.h"
 #include "editor/scene_tree_editor.h"
+#include "editor/script_create_dialog.h"
 #include "editor/settings_config_dialog.h"
 #include "scene/gui/center_container.h"
 #include "scene/gui/control.h"
@@ -56,38 +75,12 @@
 #include "scene/gui/separator.h"
 #include "scene/gui/split_container.h"
 #include "scene/gui/tab_container.h"
+#include "scene/gui/tabs.h"
 #include "scene/gui/texture_progress.h"
 #include "scene/gui/tool_button.h"
 #include "scene/gui/tree.h"
-
-#include "editor/pane_drag.h"
-
-#include "editor/editor_log.h"
-#include "editor/editor_run_script.h"
-#include "editor/project_export.h"
-#include "editor/project_settings_editor.h"
-#include "editor/resources_dock.h"
-#include "editor/run_settings_dialog.h"
-#include "editor/scene_tree_dock.h"
-#include "editor/script_create_dialog.h"
-
-#include "editor/editor_name_dialog.h"
-#include "editor/editor_plugin.h"
-#include "editor/editor_run_native.h"
-#include "editor/editor_sub_scene.h"
-#include "editor/project_export.h"
-#include "editor/quick_open.h"
-#include "editor_export.h"
-#include "editor_reimport_dialog.h"
-#include "scene/gui/tabs.h"
-
-#include "editor_resource_preview.h"
-#include "fileserver/editor_file_server.h"
 #include "scene/gui/viewport_container.h"
 
-#include "progress_dialog.h"
-
-#include "editor_scale.h"
 /**
 	@author Juan Linietsky <reduzio@gmail.com>
 */
@@ -170,7 +163,6 @@ private:
 		RUN_SETTINGS,
 		RUN_PROJECT_MANAGER,
 		RUN_FILE_SERVER,
-		//RUN_DEPLOY_DUMB_CLIENTS,
 		RUN_LIVE_DEBUG,
 		RUN_DEBUG_COLLISONS,
 		RUN_DEBUG_NAVIGATION,
@@ -181,7 +173,6 @@ private:
 		SETTINGS_UPDATE_SPINNER_HIDE,
 		SETTINGS_EXPORT_PREFERENCES,
 		SETTINGS_PREFERENCES,
-		SETTINGS_OPTIMIZED_PRESETS,
 		SETTINGS_LAYOUT_SAVE,
 		SETTINGS_LAYOUT_DELETE,
 		SETTINGS_LAYOUT_DEFAULT,
@@ -189,9 +180,6 @@ private:
 		SETTINGS_PICK_MAIN_SCENE,
 		SETTINGS_TOGGLE_FULLSCREN,
 		SETTINGS_HELP,
-		SOURCES_REIMPORT,
-		DEPENDENCY_LOAD_CHANGED_IMAGES,
-		DEPENDENCY_UPDATE_IMPORTED,
 		SCENE_TAB_CLOSE,
 
 		HELP_CLASSES,
@@ -209,10 +197,7 @@ private:
 		TOOL_MENU_BASE = 1000
 	};
 
-	//Node *edited_scene; //scene being edited
 	Viewport *scene_root; //root of the scene being edited
-
-	//Ref<ResourceImportMetadata> scene_import_metadata;
 
 	PanelContainer *scene_root_parent;
 	Control *theme_base;
@@ -247,7 +232,6 @@ private:
 	HBoxContainer *bottom_hb;
 	Control *vp_base;
 	PaneDrag *pd;
-	//PaneDrag *pd_anim;
 
 	CenterContainer *play_cc;
 	HBoxContainer *menu_hb;
@@ -269,19 +253,16 @@ private:
 	ToolButton *play_custom_scene_button;
 	ToolButton *search_button;
 	TextureProgress *audio_vu;
-	//MenuButton *fileserver_menu;
 
 	RichTextLabel *load_errors;
 	AcceptDialog *load_error_dialog;
 
-	//Control *scene_root_base;
 	Ref<Theme> theme;
 
 	PopupMenu *recent_scenes;
 	Button *property_back;
 	Button *property_forward;
 	SceneTreeDock *scene_tree_dock;
-	//ResourcesDock *resources_dock;
 	PropertyEditor *property_editor;
 	NodeDock *node_dock;
 	ImportDock *import_dock;
@@ -294,7 +275,6 @@ private:
 
 	CreateDialog *create_dialog;
 
-	//CallDialog *call_dialog;
 	ConfirmationDialog *confirmation;
 	ConfirmationDialog *save_confirmation;
 	ConfirmationDialog *import_confirmation;
@@ -308,7 +288,6 @@ private:
 	PopupMenu *editor_layouts;
 	EditorNameDialog *layout_dialog;
 
-	//OptimizedPresetsDialog *optimized_presets;
 	EditorSettingsDialog *settings_config_dialog;
 	RunSettingsDialog *run_settings_dialog;
 	ProjectSettingsEditor *project_settings;
@@ -342,8 +321,6 @@ private:
 	HBoxContainer *main_editor_button_vb;
 	Vector<ToolButton *> main_editor_buttons;
 	Vector<EditorPlugin *> editor_table;
-
-	//	EditorReImportDialog *reimport_dialog;
 
 	ProgressDialog *progress_dialog;
 	BackgroundProgress *progress_hb;
@@ -402,7 +379,6 @@ private:
 	EditorData editor_data;
 	EditorRun editor_run;
 	EditorSelection *editor_selection;
-	//	ProjectExport *project_export;
 	ProjectExportDialog *project_export;
 	EditorResourcePreview *resource_preview;
 
@@ -434,7 +410,6 @@ private:
 	void _dialog_display_file_error(String p_file, Error p_error);
 
 	int current_option;
-	//void _animation_visibility_toggle();
 	void _resource_created();
 	void _resource_selected(const RES &p_res, const String &p_property = "");
 	void _menu_option(int p_option);
@@ -450,7 +425,6 @@ private:
 
 	void _fs_changed();
 	void _sources_changed(bool p_exist);
-	void _imported(Node *p_node);
 
 	void _node_renamed();
 	void _editor_select_next();
@@ -491,7 +465,6 @@ private:
 	void _update_recent_scenes();
 	void _open_recent_scene(int p_idx);
 	void _dropped_files(const Vector<String> &p_files, int p_screen);
-	//void _open_recent_scene_confirm();
 	String _recent_scene;
 
 	bool convert_old;
@@ -501,8 +474,6 @@ private:
 	static void _load_error_notify(void *p_ud, const String &p_text);
 
 	bool has_main_screen() const { return true; }
-
-	bool _find_editing_changed_scene(Node *p_from);
 
 	String import_reload_fn;
 
@@ -580,7 +551,6 @@ private:
 	void _save_docks_to_config(Ref<ConfigFile> p_layout, const String &p_section);
 	void _load_docks_from_config(Ref<ConfigFile> p_layout, const String &p_section);
 	void _update_dock_slots_visibility();
-	void _update_top_menu_visibility();
 
 	void _update_layouts_menu();
 	void _layout_menu_option(int p_id);
@@ -700,10 +670,6 @@ public:
 
 	static EditorLog *get_log() { return singleton->log; }
 	Control *get_viewport();
-
-	//void animation_editor_make_visible(bool p_visible);
-	//void hide_animation_player_editors();
-	//void animation_panel_make_visible(bool p_visible);
 
 	void set_edited_scene(Node *p_scene);
 
@@ -860,4 +826,4 @@ struct EditorProgressBG {
 	~EditorProgressBG() { EditorNode::progress_end_task_bg(task); }
 };
 
-#endif
+#endif // EDITOR_NODE_H

editor/editor_settings.cpp
@@ -29,23 +29,23 @@
 /*************************************************************************/
 #include "editor_settings.h"
 
-#include "editor_node.h"
-#include "io/compression.h"
-#include "io/config_file.h"
-#include "io/file_access_memory.h"
-#include "io/resource_loader.h"
-#include "io/resource_saver.h"
-#include "io/translation_loader_po.h"
-#include "os/dir_access.h"
-#include "os/file_access.h"
-#include "os/keyboard.h"
-#include "os/os.h"
-#include "project_settings.h"
+#include "core/io/compression.h"
+#include "core/io/config_file.h"
+#include "core/io/file_access_memory.h"
+#include "core/io/resource_loader.h"
+#include "core/io/resource_saver.h"
+#include "core/io/translation_loader_po.h"
+#include "core/os/dir_access.h"
+#include "core/os/file_access.h"
+#include "core/os/keyboard.h"
+#include "core/os/os.h"
+#include "core/project_settings.h"
+#include "core/version.h"
+#include "editor/editor_node.h"
+#include "editor/translations.gen.h"
 #include "scene/main/node.h"
 #include "scene/main/scene_tree.h"
 #include "scene/main/viewport.h"
-#include "translations.gen.h"
-#include "version.h"
 
 Ref<EditorSettings> EditorSettings::singleton = NULL;
 
@@ -93,6 +93,7 @@ bool EditorSettings::_set(const StringName &p_name, const Variant &p_value) {
 	emit_signal("settings_changed");
 	return true;
 }
+
 bool EditorSettings::_get(const StringName &p_name, Variant &r_ret) const {
 
 	_THREAD_SAFE_METHOD_
@@ -272,7 +273,6 @@ void EditorSettings::create() {
 
 	String config_path;
 	String config_dir;
-	//String config_file="editor_settings.xml";
 	Ref<ConfigFile> extra_config = memnew(ConfigFile);
 
 	String exe_path = OS::get_singleton()->get_executable_path().get_base_dir();
@@ -644,7 +644,6 @@ void EditorSettings::_load_defaults(Ref<ConfigFile> p_extra_config) {
 	set("text_editor/files/maximum_recent_files", 20);
 	hints["text_editor/files/maximum_recent_files"] = PropertyInfo(Variant::INT, "text_editor/files/maximum_recent_files", PROPERTY_HINT_RANGE, "1, 200, 0");
 
-	//set("docks/scene_tree/display_old_action_buttons",false);
 	set("docks/scene_tree/start_create_dialog_fully_expanded", false);
 	set("docks/scene_tree/draw_relationship_lines", false);
 	set("docks/scene_tree/relationship_line_color", Color::html("464646"));
@@ -705,8 +704,6 @@ void EditorSettings::_load_defaults(Ref<ConfigFile> p_extra_config) {
 
 	set("filesystem/on_save/compress_binary_resources", true);
 	set("filesystem/on_save/save_modified_external_resources", true);
-	//set("filesystem/on_save/save_paths_as_relative",false);
-	//set("filesystem/on_save/save_paths_without_extension",false);
 
 	set("text_editor/tools/create_signal_callbacks", true);
 
@@ -1151,7 +1148,6 @@ void EditorSettings::_bind_methods() {
 
 EditorSettings::EditorSettings() {
 
-	//singleton=this;
 	last_order = 0;
 	optimize_save = true;
 	save_changed_setting = true;
@@ -1181,8 +1177,6 @@ EditorSettings::EditorSettings() {
 }
 
 EditorSettings::~EditorSettings() {
-
-	//singleton=NULL;
 }
 
 Ref<ShortCut> ED_GET_SHORTCUT(const String &p_path) {

editor/io_plugins/editor_scene_import_plugin.cpp
@@ -2095,14 +2095,6 @@ Node* EditorSceneImportPlugin::_fix_node(Node *p_node,Node *p_root,Map<Ref<Mesh>
 	return p_node;
 }
 
-#if 0
-
-Error EditorImport::import_scene(const String& p_path,const String& p_dest_path,const String& p_resource_path,uint32_t p_flags,ImageFormat p_image_format,ImageCompression p_image_compression,uint32_t p_image_flags,float p_quality,uint32_t animation_flags,Node **r_scene,Ref<EditorPostImport> p_post_import) {
-
-
-}
-#endif
-
 void EditorSceneImportPlugin::_tag_import_paths(Node *p_scene,Node *p_node) {
 
 	if (p_scene!=p_node && p_node->get_owner()!=p_scene)

commit cedbf1a4c83e1791f20978f79332bb51fb71164c

Fix inability to change project settings
Regression from 1c6c4142fe7614ade1217bfbfd644bb7d843dc38, fixes #10712.

editor/project_settings_editor.cpp
@@ -866,6 +866,12 @@ void ProjectSettingsEditor::_save() {
 	message->popup_centered(Size2(300, 100) * EDSCALE);
 }
 
+void ProjectSettingsEditor::_settings_prop_edited(const String &p_name) {
+
+	// Method needed to discard the mandatory argument of the property_edited signal
+	_settings_changed();
+}
+
 void ProjectSettingsEditor::_settings_changed() {
 
 	timer->start();
@@ -1334,6 +1340,7 @@ void ProjectSettingsEditor::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("_add_item"), &ProjectSettingsEditor::_add_item, DEFVAL(Variant()));
 	ClassDB::bind_method(D_METHOD("_device_input_add"), &ProjectSettingsEditor::_device_input_add);
 	ClassDB::bind_method(D_METHOD("_press_a_key_confirm"), &ProjectSettingsEditor::_press_a_key_confirm);
+	ClassDB::bind_method(D_METHOD("_settings_prop_edited"), &ProjectSettingsEditor::_settings_prop_edited);
 	ClassDB::bind_method(D_METHOD("_copy_to_platform"), &ProjectSettingsEditor::_copy_to_platform);
 	ClassDB::bind_method(D_METHOD("_update_translations"), &ProjectSettingsEditor::_update_translations);
 	ClassDB::bind_method(D_METHOD("_translation_delete"), &ProjectSettingsEditor::_translation_delete);
@@ -1448,7 +1455,7 @@ ProjectSettingsEditor::ProjectSettingsEditor(EditorData *p_data) {
 	globals_editor->register_search_box(search_box);
 	globals_editor->get_property_editor()->get_scene_tree()->connect("cell_selected", this, "_item_selected");
 	globals_editor->get_property_editor()->connect("property_toggled", this, "_item_checked", varray(), CONNECT_DEFERRED);
-	globals_editor->get_property_editor()->connect("property_edited", this, "_settings_changed");
+	globals_editor->get_property_editor()->connect("property_edited", this, "_settings_prop_edited");
 
 	Button *del = memnew(Button);
 	hbc->add_child(del);

editor/project_settings_editor.h
@@ -121,6 +121,7 @@ class ProjectSettingsEditor : public AcceptDialog {
 	void _press_a_key_confirm();
 	void _show_last_added(const Ref<InputEvent> &p_event, const String &p_name);
 
+	void _settings_prop_edited(const String &p_name);
 	void _settings_changed();
 
 	void _copy_to_platform(int p_which);