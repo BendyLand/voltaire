xianofantasia/Go

Core: Add `[[nodiscard]]` to string-like classes

core/string/ustring.h
@@ -87,7 +87,7 @@ public:
 /*  Char16String                                                         */
 /*************************************************************************/
 
-class Char16String {
+class [[nodiscard]] Char16String {
 	CowData<char16_t> _cowdata;
 	static const char16_t _null;
 
@@ -128,7 +128,7 @@ protected:
 /*  CharString                                                           */
 /*************************************************************************/
 
-class CharString {
+class [[nodiscard]] CharString {
 	CowData<char> _cowdata;
 	static const char _null;
 
@@ -180,7 +180,7 @@ struct StrRange {
 	}
 };
 
-class String {
+class [[nodiscard]] String {
 	CowData<char32_t> _cowdata;
 	static const char32_t _null;
 	static const char32_t _replacement_char;

BendyLand/voltaire

core/string/ustring.h
@@ -127,7 +127,7 @@ constexpr int64_t str_compare(const L *l_ptr, const R *r_ptr) {
 /*************************************************************************/
 
 template <typename T>
-class CharProxy {
+class [[nodiscard]] CharProxy {
 	friend String;
 	friend CharStringT<T>;
 
@@ -170,7 +170,7 @@ public:
 /*************************************************************************/
 
 template <typename T>
-class CharStringT {
+class [[nodiscard]] CharStringT {
 	CowData<T> _cowdata;
 	static constexpr T _null = 0;
 
@@ -265,7 +265,7 @@ using Char16String = CharStringT<char16_t>;
 /*  String                                                               */
 /*************************************************************************/
 
-class String {
+class [[nodiscard]] String {
 	CowData<char32_t> _cowdata;
 	static constexpr char32_t _null = 0;
 	static constexpr char32_t _replacement_char = 0xfffd;