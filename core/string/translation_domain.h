godotengine/godot

Add methods for querying loaded `Translation` instances

core/string/translation_domain.h
@@ -70,16 +70,22 @@ public:
 	// Methods in this section are not intended for scripting.
 	StringName get_message_from_translations(const String &p_locale, const StringName &p_message, const StringName &p_context) const;
 	StringName get_message_from_translations(const String &p_locale, const StringName &p_message, const StringName &p_message_plural, int p_n, const StringName &p_context) const;
-	PackedStringArray get_loaded_locales() const;
-	bool has_translation_for_locale(const String &p_locale) const;
-	HashSet<Ref<Translation>> get_potential_translations(const String &p_locale) const;
 
 public:
+	// These two methods are public for easier TranslationServer bindings.
+	TypedArray<Translation> get_translations_bind() const;
+	TypedArray<Translation> find_translations_bind(const String &p_locale, bool p_exact) const;
+
+#ifndef DISABLE_DEPRECATED
 	Ref<Translation> get_translation_object(const String &p_locale) const;
+#endif
 
 	void add_translation(const Ref<Translation> &p_translation);
 	void remove_translation(const Ref<Translation> &p_translation);
 	void clear();
+	const HashSet<Ref<Translation>> get_translations() const;
+	HashSet<Ref<Translation>> find_translations(const String &p_locale, bool p_exact) const;
+	bool has_translations(const String &p_locale, bool p_exact) const;
 
 	StringName translate(const StringName &p_message, const StringName &p_context) const;
 	StringName translate_plural(const StringName &p_message, const StringName &p_message_plural, int p_n, const StringName &p_context) const;

core/string/translation_server.cpp
@@ -36,6 +36,7 @@
 #include "core/os/main_loop.h"
 #include "core/os/os.h"
 #include "core/string/locales.h"
+#include "core/variant/typed_array.h"
 
 void TranslationServer::init_locale_info() {
 	// Init locale info.
@@ -489,9 +490,17 @@ String TranslationServer::get_fallback_locale() const {
 	return fallback;
 }
 
+#ifndef DISABLE_DEPRECATED
 PackedStringArray TranslationServer::get_loaded_locales() const {
-	return main_domain->get_loaded_locales();
+	PackedStringArray locales;
+	for (const Ref<Translation> &E : main_domain->get_translations()) {
+		if (!locales.has(E->get_locale())) {
+			locales.push_back(E->get_locale());
+		}
+	}
+	return locales;
 }
+#endif
 
 void TranslationServer::add_translation(const Ref<Translation> &p_translation) {
 	main_domain->add_translation(p_translation);
@@ -501,8 +510,22 @@ void TranslationServer::remove_translation(const Ref<Translation> &p_translation
 	main_domain->remove_translation(p_translation);
 }
 
+#ifndef DISABLE_DEPRECATED
 Ref<Translation> TranslationServer::get_translation_object(const String &p_locale) {
 	return main_domain->get_translation_object(p_locale);
+}
+#endif
+
+TypedArray<Translation> TranslationServer::get_translations() const {
+	return main_domain->get_translations_bind();
+}
+
+TypedArray<Translation> TranslationServer::find_translations(const String &p_locale, bool p_exact) const {
+	return main_domain->find_translations_bind(p_locale, p_exact);
+}
+
+bool TranslationServer::has_translations(const String &p_locale, bool p_exact) const {
+	return main_domain->has_translations(p_locale, p_exact);
 }
 
 void TranslationServer::clear() {
@@ -574,21 +597,27 @@ void TranslationServer::setup() {
 String TranslationServer::get_tool_locale() {
 #ifdef TOOLS_ENABLED
 	if (Engine::get_singleton()->is_editor_hint() || Engine::get_singleton()->is_project_manager_hint()) {
-		if (editor_domain->has_translation_for_locale(locale)) {
+		if (editor_domain->has_translations(locale, true)) {
 			return locale;
 		}
 		return "en";
-	} else {
-#else
-	{
-#endif
-		// Look for best matching loaded translation.
-		Ref<Translation> t = main_domain->get_translation_object(locale);
-		if (t.is_null()) {
-			return fallback;
-		}
-		return t->get_locale();
 	}
+#endif
+
+	Ref<Translation> res;
+	int best_score = 0;
+
+	for (const Ref<Translation> &E : main_domain->get_translations()) {
+		int score = TranslationServer::get_singleton()->compare_locales(locale, E->get_locale());
+		if (score > 0 && score >= best_score) {
+			res = E;
+			best_score = score;
+			if (score == 10) {
+				return locale; // Exact match.
+			}
+		}
+	}
+	return res.is_valid() ? res->get_locale() : fallback;
 }
 
 bool TranslationServer::is_pseudolocalization_enabled() const {
@@ -674,15 +703,21 @@ void TranslationServer::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("add_translation", "translation"), &TranslationServer::add_translation);
 	ClassDB::bind_method(D_METHOD("remove_translation", "translation"), &TranslationServer::remove_translation);
+
+#ifndef DISABLE_DEPRECATED
 	ClassDB::bind_method(D_METHOD("get_translation_object", "locale"), &TranslationServer::get_translation_object);
+	ClassDB::bind_method(D_METHOD("get_loaded_locales"), &TranslationServer::get_loaded_locales);
+#endif
+
+	ClassDB::bind_method(D_METHOD("get_translations"), &TranslationServer::get_translations);
+	ClassDB::bind_method(D_METHOD("find_translations", "locale", "exact"), &TranslationServer::find_translations);
+	ClassDB::bind_method(D_METHOD("has_translations", "locale", "exact"), &TranslationServer::has_translations);
 
 	ClassDB::bind_method(D_METHOD("has_domain", "domain"), &TranslationServer::has_domain);
 	ClassDB::bind_method(D_METHOD("get_or_add_domain", "domain"), &TranslationServer::get_or_add_domain);
 	ClassDB::bind_method(D_METHOD("remove_domain", "domain"), &TranslationServer::remove_domain);
 
 	ClassDB::bind_method(D_METHOD("clear"), &TranslationServer::clear);
-
-	ClassDB::bind_method(D_METHOD("get_loaded_locales"), &TranslationServer::get_loaded_locales);
 
 	ClassDB::bind_method(D_METHOD("format_number", "number", "locale"), &TranslationServer::format_number);
 	ClassDB::bind_method(D_METHOD("get_percent_sign", "locale"), &TranslationServer::get_percent_sign);

editor/editor_node.cpp
@@ -582,10 +582,9 @@ void EditorNode::_update_translations() {
 
 	if (main->is_enabled()) {
 		// Check for the exact locale.
-		// `get_potential_translations("zh_CN")` could return translations for "zh".
-		if (main->has_translation_for_locale(main->get_locale_override())) {
+		if (main->has_translations(main->get_locale_override(), true)) {
 			// The set of translation resources for the current locale changed.
-			const HashSet<Ref<Translation>> translations = main->get_potential_translations(main->get_locale_override());
+			const HashSet<Ref<Translation>> translations = main->find_translations(main->get_locale_override(), false);
 			if (translations != tracked_translations) {
 				_translation_resources_changed();
 			}
@@ -606,7 +605,7 @@ void EditorNode::_translation_resources_changed() {
 
 	const Ref<TranslationDomain> main = TranslationServer::get_singleton()->get_main_domain();
 	if (main->is_enabled()) {
-		const HashSet<Ref<Translation>> translations = main->get_potential_translations(main->get_locale_override());
+		const HashSet<Ref<Translation>> translations = main->find_translations(main->get_locale_override(), false);
 		tracked_translations.reserve(translations.size());
 		for (const Ref<Translation> &translation : translations) {
 			translation->connect_changed(callable_mp(this, &EditorNode::_queue_translation_notification));
@@ -942,7 +941,7 @@ void EditorNode::_notification(int p_what) {
 
 			// Remember the selected locale to preview node translations.
 			const String preview_locale = EditorSettings::get_singleton()->get_project_metadata("editor_metadata", "preview_locale", String());
-			if (!preview_locale.is_empty() && TranslationServer::get_singleton()->get_loaded_locales().has(preview_locale)) {
+			if (!preview_locale.is_empty() && TranslationServer::get_singleton()->has_translations(preview_locale, true)) {
 				set_preview_locale(preview_locale);
 			}
 

tests/core/string/test_translation_server.h
@@ -36,39 +36,55 @@
 
 namespace TestTranslationServer {
 TEST_CASE("[TranslationServer] Translation operations") {
+	Ref<TranslationDomain> td = TranslationServer::get_singleton()->get_or_add_domain("godot.test");
+	CHECK(td->get_translations().is_empty());
+
 	Ref<Translation> t1 = memnew(Translation);
-	t1->set_locale("uk");
+	t1->set_locale("uk"); // Ukrainian.
 	t1->add_message("Good Morning", String(U"Добрий ранок"));
+	td->add_translation(t1);
+	CHECK(td->get_translations().size() == 1);
+	CHECK(td->has_translations("uk", true));
+	CHECK(td->has_translations("uk", false));
+	CHECK_FALSE(td->has_translations("uk_UA", true));
+	CHECK(td->has_translations("uk_UA", false));
+	CHECK(td->find_translations("uk", false).size() == 1);
+	CHECK(td->find_translations("uk", true).size() == 1);
+	CHECK(td->find_translations("uk_UA", false).size() == 1);
+	CHECK(td->find_translations("uk_UA", true).size() == 0);
 
 	Ref<Translation> t2 = memnew(Translation);
-	t2->set_locale("uk");
-	t2->add_message("Hello Godot", String(U"你好戈多"));
+	t2->set_locale("uk_UA"); // Ukrainian in Ukraine.
+	t2->add_message("Hello Godot", String(U"Привіт, Годо."));
+	td->add_translation(t2);
+	CHECK(td->get_translations().size() == 2);
+	CHECK(td->has_translations("uk", true));
+	CHECK(td->has_translations("uk", false));
+	CHECK(td->has_translations("uk_UA", true));
+	CHECK(td->has_translations("uk_UA", false));
+	CHECK(td->find_translations("uk", false).size() == 2);
+	CHECK(td->find_translations("uk", true).size() == 1);
+	CHECK(td->find_translations("uk_UA", false).size() == 2);
+	CHECK(td->find_translations("uk_UA", true).size() == 1);
 
-	TranslationServer *ts = TranslationServer::get_singleton();
+	td->set_locale_override("uk");
+	CHECK(td->translate("Good Morning", StringName()) == String::utf8("Добрий ранок"));
 
-	// Adds translation for UK locale for the first time.
-	int l_count_before = ts->get_loaded_locales().size();
-	ts->add_translation(t1);
-	int l_count_after = ts->get_loaded_locales().size();
-	CHECK(l_count_after > l_count_before);
+	td->remove_translation(t1);
+	CHECK(td->get_translations().size() == 1);
+	CHECK_FALSE(td->has_translations("uk", true));
+	CHECK(td->has_translations("uk", false));
+	CHECK(td->has_translations("uk_UA", true));
+	CHECK(td->has_translations("uk_UA", false));
+	CHECK(td->find_translations("uk", true).size() == 0);
+	CHECK(td->find_translations("uk", false).size() == 1);
+	CHECK(td->find_translations("uk_UA", true).size() == 1);
+	CHECK(td->find_translations("uk_UA", false).size() == 1);
 
-	// Adds translation for UK locale again.
-	ts->add_translation(t2);
-	CHECK_EQ(ts->get_loaded_locales().size(), l_count_after);
-
-	// Removing that translation.
-	ts->remove_translation(t2);
-	CHECK_EQ(ts->get_loaded_locales().size(), l_count_after);
-
-	CHECK(ts->get_translation_object("uk").is_valid());
-
-	ts->set_locale("uk");
-	CHECK(ts->translate("Good Morning") == String::utf8("Добрий ранок"));
-
-	ts->remove_translation(t1);
-	CHECK(ts->get_translation_object("uk").is_null());
 	// If no suitable Translation object has been found - the original message should be returned.
-	CHECK(ts->translate("Good Morning") == "Good Morning");
+	CHECK(td->translate("Good Morning", StringName()) == "Good Morning");
+
+	TranslationServer::get_singleton()->remove_domain("godot.test");
 }
 
 TEST_CASE("[TranslationServer] Locale operations") {

BendyLand/voltaire

core/string/translation_domain.h
@@ -71,15 +71,24 @@ public:
 	StringName get_message_from_translations(const String &p_locale, const StringName &p_message, const StringName &p_context) const;
 	StringName get_message_from_translations(const String &p_locale, const StringName &p_message, const StringName &p_message_plural, int p_n, const StringName &p_context) const;
 	PackedStringArray get_loaded_locales() const;
-	bool has_translation_for_locale(const String &p_locale) const;
-	HashSet<Ref<Translation>> get_potential_translations(const String &p_locale) const;
 
 public:
+	// These two methods are public for easier TranslationServer bindings.
+	TypedArray<Translation> get_translations_bind() const;
+	TypedArray<Translation> find_translations_bind(const String &p_locale, bool p_exact) const;
+
+#ifndef DISABLE_DEPRECATED
 	Ref<Translation> get_translation_object(const String &p_locale) const;
+#endif
 
 	void add_translation(const Ref<Translation> &p_translation);
 	void remove_translation(const Ref<Translation> &p_translation);
 	void clear();
+
+	bool has_translation(const Ref<Translation> &p_translation) const;
+	const HashSet<Ref<Translation>> get_translations() const;
+	HashSet<Ref<Translation>> find_translations(const String &p_locale, bool p_exact) const;
+	bool has_translation_for_locale(const String &p_locale, bool p_exact) const;
 
 	StringName translate(const StringName &p_message, const StringName &p_context) const;
 	StringName translate_plural(const StringName &p_message, const StringName &p_message_plural, int p_n, const StringName &p_context) const;

core/string/translation_server.cpp
@@ -36,6 +36,7 @@
 #include "core/os/main_loop.h"
 #include "core/os/os.h"
 #include "core/string/locales.h"
+#include "core/variant/typed_array.h"
 
 void TranslationServer::init_locale_info() {
 	// Init locale info.
@@ -503,8 +504,26 @@ void TranslationServer::remove_translation(const Ref<Translation> &p_translation
 	main_domain->remove_translation(p_translation);
 }
 
+#ifndef DISABLE_DEPRECATED
 Ref<Translation> TranslationServer::get_translation_object(const String &p_locale) {
 	return main_domain->get_translation_object(p_locale);
+}
+#endif
+
+TypedArray<Translation> TranslationServer::get_translations() const {
+	return main_domain->get_translations_bind();
+}
+
+TypedArray<Translation> TranslationServer::find_translations(const String &p_locale, bool p_exact) const {
+	return main_domain->find_translations_bind(p_locale, p_exact);
+}
+
+bool TranslationServer::has_translation(const Ref<Translation> &p_translation) const {
+	return main_domain->has_translation(p_translation);
+}
+
+bool TranslationServer::has_translation_for_locale(const String &p_locale, bool p_exact) const {
+	return main_domain->has_translation_for_locale(p_locale, p_exact);
 }
 
 void TranslationServer::clear() {
@@ -576,21 +595,27 @@ void TranslationServer::setup() {
 String TranslationServer::get_tool_locale() {
 #ifdef TOOLS_ENABLED
 	if (Engine::get_singleton()->is_editor_hint() || Engine::get_singleton()->is_project_manager_hint()) {
-		if (editor_domain->has_translation_for_locale(locale)) {
+		if (editor_domain->has_translation_for_locale(locale, true)) {
 			return locale;
 		}
 		return "en";
-	} else {
-#else
-	{
-#endif
-		// Look for best matching loaded translation.
-		Ref<Translation> t = main_domain->get_translation_object(locale);
-		if (t.is_null()) {
-			return fallback;
-		}
-		return t->get_locale();
 	}
+#endif
+
+	Ref<Translation> res;
+	int best_score = 0;
+
+	for (const Ref<Translation> &E : main_domain->get_translations()) {
+		int score = TranslationServer::get_singleton()->compare_locales(locale, E->get_locale());
+		if (score > 0 && score >= best_score) {
+			res = E;
+			best_score = score;
+			if (score == 10) {
+				return locale; // Exact match.
+			}
+		}
+	}
+	return res.is_valid() ? res->get_locale() : fallback;
 }
 
 bool TranslationServer::is_pseudolocalization_enabled() const {
@@ -676,7 +701,15 @@ void TranslationServer::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("add_translation", "translation"), &TranslationServer::add_translation);
 	ClassDB::bind_method(D_METHOD("remove_translation", "translation"), &TranslationServer::remove_translation);
+
+#ifndef DISABLE_DEPRECATED
 	ClassDB::bind_method(D_METHOD("get_translation_object", "locale"), &TranslationServer::get_translation_object);
+#endif
+
+	ClassDB::bind_method(D_METHOD("get_translations"), &TranslationServer::get_translations);
+	ClassDB::bind_method(D_METHOD("find_translations", "locale", "exact"), &TranslationServer::find_translations);
+	ClassDB::bind_method(D_METHOD("has_translation_for_locale", "locale", "exact"), &TranslationServer::has_translation_for_locale);
+	ClassDB::bind_method(D_METHOD("has_translation", "translation"), &TranslationServer::has_translation);
 
 	ClassDB::bind_method(D_METHOD("has_domain", "domain"), &TranslationServer::has_domain);
 	ClassDB::bind_method(D_METHOD("get_or_add_domain", "domain"), &TranslationServer::get_or_add_domain);

editor/editor_node.cpp
@@ -585,10 +585,9 @@ void EditorNode::_update_translations() {
 
 	if (main->is_enabled()) {
 		// Check for the exact locale.
-		// `get_potential_translations("zh_CN")` could return translations for "zh".
-		if (main->has_translation_for_locale(main->get_locale_override())) {
+		if (main->has_translation_for_locale(main->get_locale_override(), true)) {
 			// The set of translation resources for the current locale changed.
-			const HashSet<Ref<Translation>> translations = main->get_potential_translations(main->get_locale_override());
+			const HashSet<Ref<Translation>> translations = main->find_translations(main->get_locale_override(), false);
 			if (translations != tracked_translations) {
 				_translation_resources_changed();
 			}
@@ -609,7 +608,7 @@ void EditorNode::_translation_resources_changed() {
 
 	const Ref<TranslationDomain> main = TranslationServer::get_singleton()->get_main_domain();
 	if (main->is_enabled()) {
-		const HashSet<Ref<Translation>> translations = main->get_potential_translations(main->get_locale_override());
+		const HashSet<Ref<Translation>> translations = main->find_translations(main->get_locale_override(), false);
 		tracked_translations.reserve(translations.size());
 		for (const Ref<Translation> &translation : translations) {
 			translation->connect_changed(callable_mp(this, &EditorNode::_queue_translation_notification));
@@ -957,7 +956,7 @@ void EditorNode::_notification(int p_what) {
 
 			// Remember the selected locale to preview node translations.
 			const String preview_locale = EditorSettings::get_singleton()->get_project_metadata("editor_metadata", "preview_locale", String());
-			if (!preview_locale.is_empty() && TranslationServer::get_singleton()->get_loaded_locales().has(preview_locale)) {
+			if (!preview_locale.is_empty() && TranslationServer::get_singleton()->has_translation_for_locale(preview_locale, true)) {
 				set_preview_locale(preview_locale);
 			}
 

tests/core/string/test_translation_server.h
@@ -36,39 +36,55 @@
 
 namespace TestTranslationServer {
 TEST_CASE("[TranslationServer] Translation operations") {
+	Ref<TranslationDomain> td = TranslationServer::get_singleton()->get_or_add_domain("godot.test");
+	CHECK(td->get_translations().is_empty());
+
 	Ref<Translation> t1 = memnew(Translation);
-	t1->set_locale("uk");
+	t1->set_locale("uk"); // Ukrainian.
 	t1->add_message("Good Morning", String(U"Добрий ранок"));
+	td->add_translation(t1);
+	CHECK(td->get_translations().size() == 1);
+	CHECK(td->has_translation_for_locale("uk", true));
+	CHECK(td->has_translation_for_locale("uk", false));
+	CHECK_FALSE(td->has_translation_for_locale("uk_UA", true));
+	CHECK(td->has_translation_for_locale("uk_UA", false));
+	CHECK(td->find_translations("uk", false).size() == 1);
+	CHECK(td->find_translations("uk", true).size() == 1);
+	CHECK(td->find_translations("uk_UA", false).size() == 1);
+	CHECK(td->find_translations("uk_UA", true).size() == 0);
 
 	Ref<Translation> t2 = memnew(Translation);
-	t2->set_locale("uk");
-	t2->add_message("Hello Godot", String(U"你好戈多"));
+	t2->set_locale("uk_UA"); // Ukrainian in Ukraine.
+	t2->add_message("Hello Godot", String(U"Привіт, Годо."));
+	td->add_translation(t2);
+	CHECK(td->get_translations().size() == 2);
+	CHECK(td->has_translation_for_locale("uk", true));
+	CHECK(td->has_translation_for_locale("uk", false));
+	CHECK(td->has_translation_for_locale("uk_UA", true));
+	CHECK(td->has_translation_for_locale("uk_UA", false));
+	CHECK(td->find_translations("uk", false).size() == 2);
+	CHECK(td->find_translations("uk", true).size() == 1);
+	CHECK(td->find_translations("uk_UA", false).size() == 2);
+	CHECK(td->find_translations("uk_UA", true).size() == 1);
 
-	TranslationServer *ts = TranslationServer::get_singleton();
+	td->set_locale_override("uk");
+	CHECK(td->translate("Good Morning", StringName()) == String::utf8("Добрий ранок"));
 
-	// Adds translation for UK locale for the first time.
-	int l_count_before = ts->get_loaded_locales().size();
-	ts->add_translation(t1);
-	int l_count_after = ts->get_loaded_locales().size();
-	CHECK(l_count_after > l_count_before);
+	td->remove_translation(t1);
+	CHECK(td->get_translations().size() == 1);
+	CHECK_FALSE(td->has_translation_for_locale("uk", true));
+	CHECK(td->has_translation_for_locale("uk", false));
+	CHECK(td->has_translation_for_locale("uk_UA", true));
+	CHECK(td->has_translation_for_locale("uk_UA", false));
+	CHECK(td->find_translations("uk", true).size() == 0);
+	CHECK(td->find_translations("uk", false).size() == 1);
+	CHECK(td->find_translations("uk_UA", true).size() == 1);
+	CHECK(td->find_translations("uk_UA", false).size() == 1);
 
-	// Adds translation for UK locale again.
-	ts->add_translation(t2);
-	CHECK_EQ(ts->get_loaded_locales().size(), l_count_after);
-
-	// Removing that translation.
-	ts->remove_translation(t2);
-	CHECK_EQ(ts->get_loaded_locales().size(), l_count_after);
-
-	CHECK(ts->get_translation_object("uk").is_valid());
-
-	ts->set_locale("uk");
-	CHECK(ts->translate("Good Morning") == String::utf8("Добрий ранок"));
-
-	ts->remove_translation(t1);
-	CHECK(ts->get_translation_object("uk").is_null());
 	// If no suitable Translation object has been found - the original message should be returned.
-	CHECK(ts->translate("Good Morning") == "Good Morning");
+	CHECK(td->translate("Good Morning", StringName()) == "Good Morning");
+
+	TranslationServer::get_singleton()->remove_domain("godot.test");
 }
 
 TEST_CASE("[TranslationServer] Locale operations") {