godotengine/godot

Rewrite Radiance and Reflection probes to use Octahedral maps.

servers/rendering/renderer_rd/environment/fog.h
@@ -170,6 +170,9 @@ private:
 			uint32_t temporal_frame;
 			float temporal_blend;
 
+			float sky_pixel_size[2];
+			float pad[2];
+
 			float cam_rotation[12];
 			float to_prev_view[16];
 			float radiance_inverse_xform[12];
@@ -325,13 +328,13 @@ public:
 		~VolumetricFog();
 	};
 
-	void init_fog_shader(uint32_t p_max_directional_lights, int p_roughness_layers, bool p_is_using_radiance_cubemap_array);
+	void init_fog_shader(uint32_t p_max_directional_lights, int p_roughness_layers, bool p_is_using_radiance_octmap_array);
 	void free_fog_shader();
 
 	struct VolumetricFogSettings {
 		Vector2i rb_size;
 		double time;
-		bool is_using_radiance_cubemap_array;
+		bool is_using_radiance_octmap_array;
 		uint32_t max_cluster_elements;
 		bool volumetric_fog_filter_active;
 		RID shadow_sampler;

BendyLand/voltaire

Co-authored-by: clayjohn <claynjohn@gmail.com>

servers/rendering/renderer_rd/environment/fog.h
@@ -182,6 +182,9 @@ private:
 			uint32_t temporal_frame;
 			float temporal_blend;
 
+			float sky_border_size[2];
+			float pad[2];
+
 			float cam_rotation[12];
 			float to_prev_view[16];
 			float radiance_inverse_xform[12];
@@ -340,13 +343,13 @@ public:
 		~VolumetricFog();
 	};
 
-	void init_fog_shader(uint32_t p_max_directional_lights, int p_roughness_layers, bool p_is_using_radiance_cubemap_array);
+	void init_fog_shader(uint32_t p_max_directional_lights, int p_roughness_layers, bool p_is_using_radiance_octmap_array);
 	void free_fog_shader();
 
 	struct VolumetricFogSettings {
 		Vector2i rb_size;
 		double time;
-		bool is_using_radiance_cubemap_array;
+		bool is_using_radiance_octmap_array;
 		uint32_t max_cluster_elements;
 		bool volumetric_fog_filter_active;
 		RID shadow_sampler;