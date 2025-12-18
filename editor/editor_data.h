godotengine/godot

Use ObjectID to store nodes in the editor selection.

editor/editor_data.h
@@ -31,6 +31,7 @@
 #pragma once
 
 #include "core/templates/list.h"
+#include "scene/main/node.h"
 #include "scene/resources/texture.h"
 
 class ConfigFile;
@@ -273,7 +274,7 @@ class EditorSelection : public Object {
 
 	// Contains the selected nodes and corresponding metadata.
 	// Metadata objects come from calling _get_editor_data on the editor_plugins, passing the selected node.
-	HashMap<Node *, Object *> selection;
+	HashMap<ObjectID, Object *> selection;
 
 	// Tracks whether the selection change signal has been emitted.
 	// Prevents multiple signals being called in one frame.
@@ -286,7 +287,7 @@ class EditorSelection : public Object {
 
 	// Editor plugins which are related to selection.
 	List<Object *> editor_plugins;
-	List<Node *> top_selected_node_list;
+	List<ObjectID> top_selected_node_list;
 
 	void _update_node_list();
 	void _emit_change();
@@ -301,10 +302,14 @@ public:
 
 	template <typename T>
 	T *get_node_editor_data(Node *p_node) {
-		if (!selection.has(p_node)) {
+		if (!p_node) {
 			return nullptr;
 		}
-		return Object::cast_to<T>(selection[p_node]);
+		ObjectID nid = p_node->get_instance_id();
+		if (!selection.has(nid)) {
+			return nullptr;
+		}
+		return Object::cast_to<T>(selection[nid]);
 	}
 
 	// Adds an editor plugin which can provide metadata for selected nodes.
@@ -315,7 +320,7 @@ public:
 
 	// Returns only the top level selected nodes.
 	// That is, if the selection includes some node and a child of that node, only the parent is returned.
-	const List<Node *> &get_top_selected_node_list();
+	List<Node *> get_top_selected_node_list();
 	// Same as get_top_selected_node_list but returns a copy in a TypedArray for binding to scripts.
 	TypedArray<Node> get_top_selected_nodes();
 	// Returns all the selected nodes (list version of "get_selected_nodes").
@@ -323,7 +328,7 @@ public:
 	// Same as get_full_selected_node_list but returns a copy in a TypedArray for binding to scripts.
 	TypedArray<Node> get_selected_nodes();
 	// Returns the map of selected objects and their metadata.
-	HashMap<Node *, Object *> &get_selection() { return selection; }
+	HashMap<ObjectID, Object *> &get_selection() { return selection; }
 
 	~EditorSelection();
 };

BendyLand/voltaire

editor/editor_data.h
@@ -31,6 +31,7 @@
 #pragma once
 
 #include "core/templates/list.h"
+#include "scene/main/node.h"
 #include "scene/resources/texture.h"
 
 class ConfigFile;
@@ -274,7 +275,7 @@ class EditorSelection : public Object {
 
 	// Contains the selected nodes and corresponding metadata.
 	// Metadata objects come from calling _get_editor_data on the editor_plugins, passing the selected node.
-	HashMap<Node *, Object *> selection;
+	HashMap<ObjectID, Object *> selection;
 
 	// Tracks whether the selection change signal has been emitted.
 	// Prevents multiple signals being called in one frame.
@@ -287,7 +288,7 @@ class EditorSelection : public Object {
 
 	// Editor plugins which are related to selection.
 	List<Object *> editor_plugins;
-	List<Node *> top_selected_node_list;
+	LocalVector<ObjectID> top_selected_node_list;
 
 	void _update_node_list();
 	void _emit_change();
@@ -302,10 +303,14 @@ public:
 
 	template <typename T>
 	T *get_node_editor_data(Node *p_node) {
-		if (!selection.has(p_node)) {
+		if (!p_node) {
 			return nullptr;
 		}
-		return Object::cast_to<T>(selection[p_node]);
+		ObjectID nid = p_node->get_instance_id();
+		if (!selection.has(nid)) {
+			return nullptr;
+		}
+		return Object::cast_to<T>(selection[nid]);
 	}
 
 	// Adds an editor plugin which can provide metadata for selected nodes.
@@ -316,7 +321,7 @@ public:
 
 	// Returns only the top level selected nodes.
 	// That is, if the selection includes some node and a child of that node, only the parent is returned.
-	const List<Node *> &get_top_selected_node_list();
+	List<Node *> get_top_selected_node_list();
 	// Same as get_top_selected_node_list but returns a copy in a TypedArray for binding to scripts.
 	TypedArray<Node> get_top_selected_nodes();
 	// Returns all the selected nodes (list version of "get_selected_nodes").
@@ -324,7 +329,7 @@ public:
 	// Same as get_full_selected_node_list but returns a copy in a TypedArray for binding to scripts.
 	TypedArray<Node> get_selected_nodes();
 	// Returns the map of selected objects and their metadata.
-	HashMap<Node *, Object *> &get_selection() { return selection; }
+	HashMap<ObjectID, Object *> &get_selection() { return selection; }
 
 	~EditorSelection();
 };