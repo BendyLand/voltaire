godotengine/godot

Restore Anchors Only preset mode

editor/scene/gui/control_editor_plugin.h
@@ -214,6 +214,7 @@ class ControlEditorToolbar : public HBoxContainer {
 	ControlEditorPopupButton *anchors_button = nullptr;
 	ControlEditorPopupButton *containers_button = nullptr;
 	Button *anchor_mode_button = nullptr;
+	CheckBox *anchors_only = nullptr;
 
 	SizeFlagPresetPicker *container_h_picker = nullptr;
 	SizeFlagPresetPicker *container_v_picker = nullptr;

BendyLand/voltaire

editor/scene/gui/control_editor_plugin.h
@@ -218,6 +218,7 @@ class ControlEditorToolbar : public HBoxContainer {
 	ControlEditorPopupButton *anchors_button = nullptr;
 	ControlEditorPopupButton *containers_button = nullptr;
 	Button *anchor_mode_button = nullptr;
+	CheckBox *reposition_button = nullptr;
 
 	AnchorPresetPicker *anchors_picker = nullptr;
 