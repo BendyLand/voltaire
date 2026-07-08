Godngine/godot

Generic6DOFJoint3D: Rename FLAG_ENABLE_MOTOR to FLAG_ENABLE_ANGULAR_MOTOR

modules/godot_physics_3d/joints/godot_generic_6dof_joint_3d.cpp
@@ -629,7 +629,7 @@ void GodotGeneric6DOFJoint3D::set_flag(Vector3::Axis p_axis, PhysicsServer3D::G6
 		case PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_LIMIT: {
 			m_angularLimits[p_axis].m_enableLimit = p_value;
 		} break;
-		case PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_MOTOR: {
+		case PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_MOTOR: {
 			m_angularLimits[p_axis].m_enableMotor = p_value;
 		} break;
 		case PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_LINEAR_MOTOR: {
@@ -655,7 +655,7 @@ bool GodotGeneric6DOFJoint3D::get_flag(Vector3::Axis p_axis, PhysicsServer3D::G6
 		case PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_LIMIT: {
 			return m_angularLimits[p_axis].m_enableLimit;
 		} break;
-		case PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_MOTOR: {
+		case PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_MOTOR: {
 			return m_angularLimits[p_axis].m_enableMotor;
 		} break;
 		case PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_LINEAR_MOTOR: {

scene/3d/physics/joints/generic_6dof_joint_3d.h
@@ -68,7 +68,8 @@ public:
 		FLAG_ENABLE_ANGULAR_LIMIT = PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_LIMIT,
 		FLAG_ENABLE_LINEAR_SPRING = PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_LINEAR_SPRING,
 		FLAG_ENABLE_ANGULAR_SPRING = PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_SPRING,
-		FLAG_ENABLE_MOTOR = PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_MOTOR,
+		FLAG_ENABLE_ANGULAR_MOTOR = PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_MOTOR,
+		FLAG_ENABLE_MOTOR = PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_MOTOR,
 		FLAG_ENABLE_LINEAR_MOTOR = PhysicsServer3D::G6DOF_JOINT_FLAG_ENABLE_LINEAR_MOTOR,
 		FLAG_MAX = PhysicsServer3D::G6DOF_JOINT_FLAG_MAX
 	};

BendyLand/voltaire

modules/godot_physics_3d/joints/godot_generic_6dof_joint_3d.cpp
@@ -641,7 +641,7 @@ void GodotGeneric6DOFJoint3D::set_flag(Vector3::Axis p_axis, PS3DE::G6DOFJointAx
 		case PS3DE::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_LIMIT: {
 			m_angularLimits[p_axis].m_enableLimit = p_value;
 		} break;
-		case PS3DE::G6DOF_JOINT_FLAG_ENABLE_MOTOR: {
+		case PS3DE::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_MOTOR: {
 			m_angularLimits[p_axis].m_enableMotor = p_value;
 		} break;
 		case PS3DE::G6DOF_JOINT_FLAG_ENABLE_LINEAR_MOTOR: {
@@ -667,7 +667,7 @@ bool GodotGeneric6DOFJoint3D::get_flag(Vector3::Axis p_axis, PS3DE::G6DOFJointAx
 		case PS3DE::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_LIMIT: {
 			return m_angularLimits[p_axis].m_enableLimit;
 		} break;
-		case PS3DE::G6DOF_JOINT_FLAG_ENABLE_MOTOR: {
+		case PS3DE::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_MOTOR: {
 			return m_angularLimits[p_axis].m_enableMotor;
 		} break;
 		case PS3DE::G6DOF_JOINT_FLAG_ENABLE_LINEAR_MOTOR: {

scene/3d/physics/joints/generic_6dof_joint_3d.h
@@ -69,7 +69,10 @@ public:
 		FLAG_ENABLE_ANGULAR_LIMIT = PS3DE::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_LIMIT,
 		FLAG_ENABLE_LINEAR_SPRING = PS3DE::G6DOF_JOINT_FLAG_ENABLE_LINEAR_SPRING,
 		FLAG_ENABLE_ANGULAR_SPRING = PS3DE::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_SPRING,
-		FLAG_ENABLE_MOTOR = PS3DE::G6DOF_JOINT_FLAG_ENABLE_MOTOR,
+		FLAG_ENABLE_ANGULAR_MOTOR = PS3DE::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_MOTOR,
+#ifndef DISABLE_DEPRECATED
+		FLAG_ENABLE_MOTOR = PS3DE::G6DOF_JOINT_FLAG_ENABLE_ANGULAR_MOTOR,
+#endif // DISABLE_DEPRECATED
 		FLAG_ENABLE_LINEAR_MOTOR = PS3DE::G6DOF_JOINT_FLAG_ENABLE_LINEAR_MOTOR,
 		FLAG_MAX = PS3DE::G6DOF_JOINT_FLAG_MAX
 	};